bd121aae7598f37b1b2ed04837b4472303c3f20f
CaligoArts <88352999+CaligoArts@users.noreply.github.com>
2021-09-07 15:18:11 -0400

    Added GameManager.cs to handle scene transition
    also updated HelloWorld notes.

--- MyScripts/HelloWorld.cs
+++ MyScripts/HelloWorld.cs
@@ -2,6 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;  //Gives ability to switch between scenes.
+using UnityEngine.UI;   //Access UI elements.
+
+//If using Editor then it applies the code to quit testing otherwise is applies code to exit built game:
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 // My 1st CSharp script :) I will store all coding notes here.
 
 public class HelloWorld : MonoBehaviour     // This must always match name of the script, If renaming script make sure to rename the public class also.
@@ -323,5 +331,43 @@ public class TwoPlayerController : MonoBehaviour
             mainCamera.enabled = !mainCamera.enabled;
             fpvCamera.enabled = !fpvCamera.enabled;
         }
+    }
+}
+
+public class GameManager : MonoBehaviour
+//Create an Empty GameObject to attach script to so you can target it in the OnClick function of the Button.
+//** Must include namespaces at top:
+//using UnityEngine.SceneManagement;  //Gives ability to switch between scenes.
+//using UnityEngine.UI;   //Access UI elements.
+
+//If using Editor then it applies the code to quit testing otherwise is applies code to exit built game:
+//#if UNITY_EDITOR
+//using UnityEditor;
+//#endif
+
+{
+    //These are Methods for Buttons to access to give them scene control:
+    public void OnePlayer()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void TwoPlayer()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();   //Exits Playmode when using Editor.
+#else
+        Application.Quit();     //Exits application after built.
+#endif
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
